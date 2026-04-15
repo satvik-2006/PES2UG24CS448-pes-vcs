487a31c1a4126e6b729ba8e40c2c0ebf6dc612be
satvik-2006 <satvikdas2006@gmail.com>
2026-04-15 02:54:13 -0400

    Improve readability of object buffer handling

--- object.c
+++ object.c
@@ -103,7 +103,7 @@ int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out
     int header_len = sprintf(header, "%s %zu", type_str, len) + 1;
 
     size_t total_len = header_len + len;
-    unsigned char *buffer = malloc(total_len);
+    unsigned char *object_buf = malloc(total_len);
     if (!buffer) return -1;
 
     memcpy(buffer, header, header_len);
