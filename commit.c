405247072c9e0fc437e4beba220a6063159142d6
satvik-2006 <satvikdas2006@gmail.com>
2026-04-15 08:25:22 -0400

    Start Phase 4 commit object implementation

--- commit.c
+++ commit.c
@@ -193,6 +193,8 @@ int head_update(const ObjectID *new_commit) {
 //   - head_update       : moves the branch pointer to your new commit
 //
 // Returns 0 on success, -1 on error.
+/* Create a commit from the current staged snapshot and update HEAD */
+
 int commit_create(const char *message, ObjectID *commit_id_out) {
     // TODO: Implement commit creation
     // (See Lab Appendix for logical steps)
